a3b0ad193aad42acecb3dceb0d74e251fe3c85b5
JORGE MIKAEL COUTINHO ALVES <94020368+JorgeCoutinho@users.noreply.github.com>
2025-02-06 23:48:44 -0300

    CRUD de Pessoa: criando o findAll, findOne (Read) e remove (Delete)

--- src/pessoas/entities/pessoa.entity.ts
+++ src/pessoas/entities/pessoa.entity.ts
@@ -1,24 +1,30 @@
-import { IsEmail } from "class-validator";
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Pessoa {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @IsEmail()
-    email: string;
+  @Column({unique: true})
+  @IsEmail()
+  email: string;
 
-    @Column({length: 255})
-    passwordHash: string;
+  @Column({ length: 255 })
+  passwordHash: string;
 
-    @Column({length: 100})
-    nome: string;
+  @Column({ length: 100 })
+  nome: string;
 
-    @CreateDateColumn()
-    createAt?: Date;
+  @CreateDateColumn()
+  createAt?: Date;
 
-    @UpdateDateColumn()
-    updateAt?: Date;
+  @UpdateDateColumn()
+  updateAt?: Date;
 }
--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pessoa } from './entities/pessoa.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PessoasService {
-  create(createPessoaDto: CreatePessoaDto) {
-    return 'This action adds a new pessoa';
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+  ) { }
+
+  async create(createPessoaDto: CreatePessoaDto) {
+
+    try {
+      const dadosPessoa = {
+        nome: createPessoaDto.nome,
+        passwordHash: createPessoaDto.password,
+        email: createPessoaDto.email,
+      }
+
+      const novaPessoa = this.pessoaRepository.create(dadosPessoa);
+      await this.pessoaRepository.save(novaPessoa);
+      return novaPessoa;
+    } catch (error) {
+      if (error.code === '23505') {
+        return { error: 'Email já cadastrado' };
+      }
+
+      throw error;
+    }
+
+
   }
 
-  findAll() {
-    return `This action returns all pessoas`;
+  async findAll() {
+    const pessoas = await this.pessoaRepository.find({
+      order: {
+        id: 'DESC'
+      }
+    });
+
+    return pessoas;
   }
 
   findOne(id: number) {
@@ -20,7 +53,15 @@ export class PessoasService {
     return `This action updates a #${id} pessoa`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pessoa`;
+  async remove(id: number) {
+    const person = await this.pessoaRepository.findOneBy({
+      id,
+    });
+
+    if (!person) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return this.pessoaRepository.remove(person);
   }
 }
